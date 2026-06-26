a596a647c4f5e5182a5e71b6d8f7ce39672d4a32
JYRAVLENOK <ivasiliev812@gmail.com>
2026-06-26 14:39:49 +0700

    fix mysql compose

--- marzban.sh
+++ marzban.sh
@@ -911,8 +911,7 @@ ${marzban_ssl_volumes}
       - --host-cache-size=0                       # Disables host cache to prevent DNS issues
       - --innodb-open-files=1024                  # Sets the limit for InnoDB open files
       - --innodb-buffer-pool-size=256M            # Allocates buffer pool size for InnoDB
-      - --innodb-log-file-size=64M                # Sets InnoDB log file size to balance log retention and performance
-      - --innodb-log-files-in-group=2             # Uses two log files to balance recovery and disk I/O
+      - --innodb-redo-log-capacity=128M           # Redo log capacity (replaces innodb_log_file_size + innodb_log_files_in_group since MySQL 8.0.30)
       - --general_log=0                           # Disables general query log for lower disk usage
       - --slow_query_log=1                        # Enables slow query log for performance analysis
       - --slow_query_log_file=/var/lib/mysql/slow.log # Logs slow queries for troubleshooting
